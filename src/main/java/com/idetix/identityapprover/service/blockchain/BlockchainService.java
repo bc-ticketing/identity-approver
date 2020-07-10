04e7282e76ef484186772ad72ad1a76602755fa0
SpielmannCode <23359516+SpielmannCode@users.noreply.github.com>
2020-07-10 17:25:38 +0200

    added Function to check Sec Level of Eth Address

--- src/main/java/com/idetix/identityapprover/service/blockchain/BlockchainService.java
+++ src/main/java/com/idetix/identityapprover/service/blockchain/BlockchainService.java
@@ -1,5 +1,8 @@
 package com.idetix.identityapprover.service.blockchain;
 
+import org.web3j.abi.datatypes.generated.Uint256;
+
 public interface BlockchainService {
-    public boolean SaveIdentityProofToChain(String ethAddress, int securityLevel);
+    public boolean saveIdentityProofToChain(String ethAddress, int securityLevel);
+    public int getSecurityLevelforAdress (String ethAddress);
 }
--- src/main/java/com/idetix/identityapprover/service/blockchain/BlockchainServiceImpl.java
+++ src/main/java/com/idetix/identityapprover/service/blockchain/BlockchainServiceImpl.java
@@ -1,16 +1,24 @@
 package com.idetix.identityapprover.service.blockchain;
 
+import jnr.ffi.annotations.In;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.web3j.abi.FunctionEncoder;
+import org.web3j.abi.FunctionReturnDecoder;
+import org.web3j.abi.TypeReference;
 import org.web3j.abi.datatypes.Address;
 import org.web3j.abi.datatypes.Function;
+import org.web3j.abi.datatypes.Type;
 import org.web3j.abi.datatypes.Uint;
+import org.web3j.abi.datatypes.generated.Uint256;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.WalletUtils;
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.request.Transaction;
+import org.web3j.protocol.core.methods.response.EthCall;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.tx.RawTransactionManager;
@@ -22,6 +30,7 @@ import org.web3j.tx.response.TransactionReceiptProcessor;
 import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 
 @Service
@@ -43,7 +52,7 @@ public class BlockchainServiceImpl implements BlockchainService {
     }
 
     @Override
-    public boolean SaveIdentityProofToChain(String ethAddress, int securityLevel) {
+    public boolean saveIdentityProofToChain(String ethAddress, int securityLevel) {
         try {
             Function function = new Function("approveIdentity", // Function name
                     Arrays.asList(new Address(ethAddress), new Uint(BigInteger.valueOf(securityLevel))), // Function input parameters
@@ -69,6 +78,27 @@ public class BlockchainServiceImpl implements BlockchainService {
             return false;
         }
         return true;
+    }
+    @Override
+    public int getSecurityLevelforAdress (String ethAddress) {
+        try {
+            Function function = new Function("getSecurityLevel", // Function name
+                    Arrays.asList(new Address(credentials.getAddress()), new Address(ethAddress)), // Function input parameters
+                    Collections.singletonList(new TypeReference<Uint256>() {})); // Function returned parameters
+
+            String encodedFunction = FunctionEncoder.encode(function);
+            EthCall response = web3.ethCall(
+                    Transaction.createEthCallTransaction(credentials.getAddress(), contractAddress, encodedFunction),
+            DefaultBlockParameterName.LATEST).sendAsync().get();
+
+            List<Type> someTypes = FunctionReturnDecoder.decode(
+                    response.getValue(), function.getOutputParameters());
+            Uint256 uint256 = (Uint256) someTypes.get(0);
+            return uint256.getValue().intValue();
+
+        } catch (Exception e) {
+            return -1;
+        }
     }
 
 
